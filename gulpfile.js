7ac0383f5a73bbccc353633c14ca7b691b23796c
luixaviles <luis.avilesz@gmail.com>
2017-10-11 17:52:47 -0400

    Adding watch task

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
 var gulp = require("gulp");
 var ts = require("gulp-typescript");
-var tsProject = ts.createProject("tsconfig.json");
+var tsProject = ts.createProject("tsconfig.json", { noEmitOnError: true });
 
 gulp.task("build", function () {
-    return tsProject.src()
-        .pipe(tsProject())
-        .js.pipe(gulp.dest("./dist"));
+    var tsResult = tsProject.src()
+        .pipe(tsProject());
+
+    return tsResult.js.pipe(gulp.dest("./dist"));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('./src/**/*.ts', ['build']);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
-let helloWorld:string = 'Hello TypeScript world';
+let helloWorld:string = 'Hello TypeScript worlds';
+let numberContainer:number = 2;
+// numberContainer.trim();
 console.log(helloWorld);
